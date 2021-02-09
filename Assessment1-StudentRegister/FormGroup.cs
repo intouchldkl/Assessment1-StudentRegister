88372a69cdf14dac8d3154ffbc00a72342068dde
inn@ascot.ac.th <inn@ascot.ac.th>
2021-02-09 14:58:58 +0700

    Intouch

--- Assessment1-StudentRegister/FormGroup.cs
+++ Assessment1-StudentRegister/FormGroup.cs
@@ -6,5 +6,29 @@ namespace Assessment1_StudentRegister
 {
     class FormGroup
     {
+        private string FormName;
+        private string FormTeacherName;
+        private Student[] students;
+        private int TotalStudent;
+
+        public FormGroup(string FormName,string FormTeacherName)
+        {
+            this.FormName = FormName;
+            this.FormTeacherName = FormTeacherName;
+            students = new Student[20];
+            TotalStudent = 0;
+        }
+
+        public void AddStudent(string StudentName,DateTime DOB,bool Gender)
+        {
+            students[TotalStudent] = new Student(StudentName, DOB, Gender);
+            TotalStudent++;
+
+        }
+        public void TakeRegister()
+        {
+
+        }
+
     }
 }
--- Assessment1-StudentRegister/Student.cs
+++ Assessment1-StudentRegister/Student.cs
@@ -10,6 +10,11 @@ namespace Assessment1_StudentRegister
         private DateTime DOB;
         private bool Gender;
         private Attendance[] attendances;
+        private int attendanceCount;
+        private int TotalPresence;
+        private int TotalAbsence;
+        private int TotalLate;
+        private int TotalMinLate;
 
         public Student(string StudentName,DateTime DOB,bool Gender)
         {
@@ -17,10 +22,78 @@ namespace Assessment1_StudentRegister
             this.DOB = DOB;
             this.Gender = Gender;
           attendances = new Attendance[180];
+            attendanceCount = 0;
+            TotalPresence = 0;
+            TotalAbsence = 0;
+            TotalLate = 0;
+            TotalMinLate = 0;
         }
-        public void AddAttendence()
+       
+        public void AddAttendence(DateTime Date,string AttendenceStatus)
+        { 
+            
+            
+         
+            if(AttendenceStatus == "P")
+            {
+                TotalPresence++;
+                attendances[attendanceCount] = new Attendance(Date, AttendenceStatus);
+                attendanceCount++;
+            }
+            else if(AttendenceStatus == "A")
+            {
+                TotalAbsence++;
+                attendances[attendanceCount] = new Attendance(Date, AttendenceStatus);
+                attendanceCount++;
+            }
+            else if(AttendenceStatus == "L")
+            {
+                TotalLate++;
+                attendances[attendanceCount] = new Attendance(Date, AttendenceStatus);
+                TotalMinLate = TotalMinLate + attendances[attendanceCount].GetMinutesLate();
+                
+                attendanceCount++;
+            }
+            else
+            {
+                Console.WriteLine("Ineligible Attendance status");
+            }
+          
+        }
+        public int getTotalPresence()
         {
-            Attendances[] = new Attendance(DateTime.Now, "L");
+            return TotalPresence;
         }
+        public int getTotalAbsence()
+        {
+            return TotalAbsence;
+        }
+        public int getTotalLate()
+        {
+            return TotalLate;
+        }
+        public int getTotalMinLate()
+        {
+            return TotalMinLate;
+        }
+        public string getStudentAttendanceOnDate(DateTime Date)
+        {
+            string attendanceOnDate = "No data on that date";
+            for(int i = 0;i < attendances.Length;i++)
+            {
+                if(attendances[i] == null)
+                {
+                    return attendanceOnDate;
+                }
+                
+                if(attendances[i].getDate() == Date)
+                {
+                    attendanceOnDate = attendances[i].GetAttendanceStatus();
+                }
+            }
+            return attendanceOnDate;
+        }
+
+
     }
 }
